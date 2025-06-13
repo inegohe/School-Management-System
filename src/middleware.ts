d2036f32f6a974dc30e384d93a442d258502e8db
U22099 <nifemiolaniyi4@gmail.com>
2025-06-13 14:28:15 +0100

    update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,13 +6,8 @@ const publicOnlyRoutes = ["/", "/login", "/create", "/newschool"];
 const genericAuthenticatedRoute = "/auth";
 
 export async function middleware(req: NextRequest): Promise<NextResponse> {
-  const origin = req.headers.get("origin");
-  const forwardedHost = req.headers.get("x-forwarded-host");
-  const cookieStore = await cookies();
-  const refreshToken =
-    cookieStore.get("refreshtoken")?.value ||
-    req.cookies.get("refreshtoken")?.value ||
-    "";
+  const cookieStore = cookies();
+  const refreshToken = cookieStore.get("refreshtoken")?.value req.cookies.get(“refreshtoken”)?.value || "";
   const url = req.nextUrl.clone();
   const requestedPath = url.pathname;
 
@@ -20,9 +15,6 @@ export async function middleware(req: NextRequest): Promise<NextResponse> {
 
   if (!refreshToken) {
     if (!isPublicOnlyRoute) {
-      console.log(
-        `Middleware: No token, redirecting from protected route ${requestedPath} to /login`
-      );
       url.pathname = "/login";
       url.searchParams.set("callbackUrl", requestedPath);
       return NextResponse.redirect(url);
@@ -30,9 +22,6 @@ export async function middleware(req: NextRequest): Promise<NextResponse> {
     return NextResponse.next();
   } else {
     if (isPublicOnlyRoute) {
-      console.log(
-        `Middleware: Logged-in user redirected from public-only route ${requestedPath} to ${genericAuthenticatedRoute}`
-      );
       url.pathname = genericAuthenticatedRoute;
       url.search = "";
       return NextResponse.redirect(url);
@@ -43,5 +32,21 @@ export async function middleware(req: NextRequest): Promise<NextResponse> {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - /android, /ios, /window11 (specific directories)
+     * - favicon.ico (favicon file)
+     * - icons (PWA icons directory)
+     * - manifest.json (PWA manifest file)
+     * - sw.js (service worker file)
+     * - sw.ts (service worker file)
+     * - image files (png, jpg, jpeg)
+     * - excel files (xlsx)
+     */
+    "/((?!api|_next/static|_next/image|/android|/ios|/windows11|favicon\\.ico|icons|manifest\\.json|sw\\.js|sw\\.ts|.*\\.(png|jpg|jpeg|xlsx)$).*)",
+  ],
 };
