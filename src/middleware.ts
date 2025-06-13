06d082fcfb642a70d70603966a9a7e41a0a4eba0
U22099 <nifemiolaniyi4@gmail.com>
2025-06-13 14:53:03 +0100

    update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -49,6 +49,6 @@ export const config = {
      * - favicon.ico (favicon file)
      * Feel free to modify this pattern to include more paths.
      */
-    '/((?!_next/static|_next/image|manifest.json|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next/static|_next/image|manifest.json|sw.js|sw.ts|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
