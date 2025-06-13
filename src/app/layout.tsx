4fe60873aa1af38447ef20097bcc0ec2daa1c411
U22099 <nifemiolaniyi4@gmail.com>
2025-06-13 11:33:47 +0100

    update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
